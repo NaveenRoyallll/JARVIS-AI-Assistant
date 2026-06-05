80f0cbd6cccefce248e2a4523aff278e8f301199
VNR Naveen <201685102+VNRNaveen8055@users.noreply.github.com>
2026-06-05 18:03:12 +0530

    updating Jarvis Code

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
-
+#pip install pocketsphinx
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 def speak(text):
     engine.say(text)
     engine.runAndWait()
-
+def processcommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")
 
 if __name__ == "__main__":
     speak("Initializing Jarvis....")
@@ -17,19 +25,22 @@ if __name__ == "__main__":
 
         r = sr.Recognizer()
 
-        with sr.Microphone() as source:
-            print("listening...")
-            audio = r.listen(source)
-
         # recognize speech using Sphinx
+        print("Recognizing....")
         try:
-            print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-
-        except sr.UnknownValueError:
-            print("Sphinx could not understand audio")
-
-        except sr.RequestError as e:
-            print("Sphinx error: {0}".format(e))
+            with sr.Microphone() as source:
+                print("listening...")
+                audio = r.listen(source,timeout=2,phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower()=="jarvis"):
+                speak("Yeah")
+                with sr.Microphone() as source:
+                    print("Jarvis Active")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+                    processcommand(command)
+        except Exception as e:
+            print("Error; {0}".format(e))
 
 
 
